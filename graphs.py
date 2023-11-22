94c751dba1b6bac8c0f6d2d5334678b4c387411b
root <adamdowse@hotmail.co.uk>
2023-11-22 21:05:01 +0000

    new save

--- graphs.py
+++ graphs.py
@@ -44,37 +44,66 @@ plt.rcParams.update({
     "ytick.labelsize": 8
 })
 
+sea.
+
 
 #OriginalFIM
-OriginalFIM = pd.read_csv('FIMOriginal.csv', sep=',')
+def make_OGFIM():
+    OriginalFIM = pd.read_csv('FIMOriginal.csv', sep=',')
 
-print(OriginalFIM.head())
+    print(OriginalFIM.head())
 
-#bar plot of OriginalFIM with maximum and minimum values
-fig, ax = plt.subplots(1,1,figsize=set_size(252))
-sea.set(style="whitegrid")
-sea.barplot(x=["ResNet-18","5 Layer CNN","8 Layer CNN","ResNetV1-14"],
-            y=[OriginalFIM.loc[0,'T4'],OriginalFIM.loc[0,'T3'],OriginalFIM.loc[0,'T2'],OriginalFIM.loc[0,'T1']],
-            palette="Blues_d",
+    #bar plot of OriginalFIM with maximum and minimum values
+    fig, ax = plt.subplots(1,1,figsize=set_size(252))
+    sea.set(style="whitegrid")
+    sea.barplot(x=["ResNet-18","5 Layer CNN","8 Layer CNN","ResNetV1-14"],
+                y=[OriginalFIM.loc[0,'T4'],OriginalFIM.loc[0,'T3'],OriginalFIM.loc[0,'T2'],OriginalFIM.loc[0,'T1']],
+                palette="Blues_d",
+                )
+    plt.ylabel("Original FIM")
+    plt.xlabel("Models")
+    plt.xticks(rotation=15)
+    plt.grid(axis='y',which='both',linestyle='--')
+    ax.set_axisbelow(True)
+
+
+    #max min error bars
+    names = ["T4","T3","T2","T1"]
+    i = 0
+    for p in ax.patches:
+        print(names[i]+"_min")
+        plt.vlines(p.get_x()+p.get_width()/2,
+            OriginalFIM.loc[0,names[i]+"_min"],
+            OriginalFIM.loc[0,names[i]+"_max"],
+            color='black',
+            alpha=0.5,
             )
-plt.ylabel("Original FIM")
-plt.xlabel("Models")
-plt.xticks(rotation=15)
-plt.grid(axis='y',which='both',linestyle='--')
-ax.set_axisbelow(True)
+        i+=1
+    #save the plot
+    fig.savefig("Pics/OGFIM.png", bbox_inches='tight', dpi=500)
 
+def make_T1GFIM():
+    T1FIMstep = pd.read_csv('T1_step.csv', sep=',')
+    T1FIMstep = T1FIMstep.iloc[::4]
+    print(T1FIMstep.head())
 
-#max min error bars
-names = ["T4","T3","T2","T1"]
-i = 0
-for p in ax.patches:
-    print(names[i]+"_min")
-    plt.vlines(p.get_x()+p.get_width()/2,
-        OriginalFIM.loc[0,names[i]+"_min"],
-        OriginalFIM.loc[0,names[i]+"_max"],
-        color='black',
-        alpha=0.5,
-        )
-    i+=1
-#save the plot
-fig.savefig("OGFIM.png", bbox_inches='tight', dpi=500)
+    fig, (ax1,ax2) = plt.subplots(1,2,figsize=set_size(516,0.5))
+    sea.set(style="whitegrid")
+    
+    sea.lineplot(ax=ax1,x="Step",y="0",data=T1FIMstep,label="0 (Low)",linewidth=0.5,color="")
+    sea.lineplot(ax=ax1,x="Step",y="1",data=T1FIMstep,label="1",linewidth=0.5)
+    sea.lineplot(ax=ax1,x="Step",y="2",data=T1FIMstep,label="2",linewidth=0.5)
+    sea.lineplot(ax=ax1,x="Step",y="3",data=T1FIMstep,label="3",linewidth=0.5)
+    sea.lineplot(ax=ax1,x="Step",y="4",data=T1FIMstep,label="4",linewidth=0.5)
+    sea.lineplot(ax=ax1,x="Step",y="5",data=T1FIMstep,label="5",linewidth=0.5)
+    sea.lineplot(ax=ax1,x="Step",y="6",data=T1FIMstep,label="6",linewidth=0.5)
+    sea.lineplot(ax=ax1,x="Step",y="7",data=T1FIMstep,label="7 (High)",linewidth=0.5)
+    
+    plt.ylabel("T1 GFIM")
+    plt.xlabel("Step")
+    ax1.get_legend().remove()
+
+    fig.savefig("Pics/T1_GFIM.png", bbox_inches='tight', dpi=500)
+    #T1FIMepoch = pd.read_csv('T1_epoch.csv', sep=',')
+
+make_T1GFIM()
